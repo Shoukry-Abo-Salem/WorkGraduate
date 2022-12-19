c9ed68460c7bbaef7fb5b01891ce16f4d3b69ba6
Compulife <shoukry454@gmail.com>
2022-12-19 16:24:19 +0200

    gradee

--- app/src/main/java/com/shoukry/workgraduate/OrdersFragment.java
+++ app/src/main/java/com/shoukry/workgraduate/OrdersFragment.java
@@ -12,16 +12,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.tabs.TabLayout;
+import com.shoukry.workgraduate.AdapterFragments.DetailsAdapter;
+import com.shoukry.workgraduate.AdapterFragments.LoginFragmentAdapter;
 import com.shoukry.workgraduate.databinding.FragmentOrdersBinding;
+import com.shoukry.workgraduate.details.CompletedFragment;
+import com.shoukry.workgraduate.details.PendingFragment;
+import com.shoukry.workgraduate.details.UnderwayFragment;
+
+import java.util.ArrayList;
 
 
 public class OrdersFragment extends Fragment {
     FragmentOrdersBinding binding;
+    LoginFragmentAdapter adapter;
 
     public OrdersFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,14 @@ public class OrdersFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        ArrayList<Fragment> arrayList = new ArrayList<>();
+        arrayList.add(new PendingFragment());
+        arrayList.add(new CompletedFragment());
+        arrayList.add(new UnderwayFragment());
+
+        adapter = new LoginFragmentAdapter(getActivity(),arrayList);
+        binding.ordersViewPager.setAdapter(adapter);
+
         binding = FragmentOrdersBinding.inflate(inflater,container,false);
         binding.tabLayoutService.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
