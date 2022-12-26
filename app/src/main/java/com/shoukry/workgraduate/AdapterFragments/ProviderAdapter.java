6240800aae5b6f19727ec57471b217a3edd653d9
Compulife <shoukry454@gmail.com>
2022-12-26 17:32:10 +0200

    gradee

--- app/src/main/java/com/shoukry/workgraduate/AdapterFragments/ProviderAdapter.java
+++ app/src/main/java/com/shoukry/workgraduate/AdapterFragments/ProviderAdapter.java
@@ -1,9 +1,11 @@
 package com.shoukry.workgraduate.AdapterFragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -41,6 +43,15 @@ public class ProviderAdapter extends RecyclerView.Adapter<ProviderAdapter.Provid
         holder.binding.txtUserName.setText(providerModel.getName());
         holder.binding.txtOrderId.setText(String.valueOf(providerModel.getId()));
         holder.binding.imageUser.setImageResource(providerModel.getImg());
+
+        holder.binding.btnDetails.setOnClickListener(View ->{
+            Intent intent = new Intent();
+            Toast.makeText(context, "Provider Clicked", Toast.LENGTH_SHORT).show();
+//            intent.putExtra("name",providerModel.getName());
+//            intent.putExtra("image",providerModel.getImg());
+//            intent.putExtra("location",providerModel.);
+//            intent.putExtra("",providerModel.getName());
+        });
     }
 
     @Override
